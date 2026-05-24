10d5327d57e094a6f90a8c8756bdaf9f2996acf4
Nikita Krivoshei <69799139+itkrivoshei@users.noreply.github.com>
2026-05-24 15:57:11 +0200

    fix: use esm import in svg declaration

--- src/declarations.d.ts
+++ src/declarations.d.ts
@@ -14,7 +14,8 @@ declare module '*.mp3' {
 }
 
 declare module '*.svg' {
-  import React = require('react');
+  import * as React from 'react';
+
   export const ReactComponent: React.FunctionComponent<
     React.SVGProps<SVGSVGElement>
   >;
