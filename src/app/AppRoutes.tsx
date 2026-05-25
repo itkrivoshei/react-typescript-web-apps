fd2f0c02a546e7956c5256be9bfcf6f9b18dd389
Nikita Krivoshei <69799139+itkrivoshei@users.noreply.github.com>
2026-05-25 16:38:23 +0200

    refactor: route weather from feature folder

--- src/app/AppRoutes.tsx
+++ src/app/AppRoutes.tsx
@@ -4,6 +4,7 @@ import { Routes, Route, useLocation } from 'react-router-dom';
 import AppNavigationMenu from './AppNavigationMenu';
 import Dashboard from '../features/dashboard/Dashboard';
 import Calculator from '../features/calculator';
+import WeatherApp from '../features/weather';
 import Restaurant from '../components/RestaurantPage/Restaurant';
 import TodoApp from '../components/TodoList/TodoApp';
 import TicTacToe from '../components/TicTacToe/TicTacToe';
@@ -15,7 +16,6 @@ import RockPaperScissors from '../components/RockPaperScissors/RockPaperScissors
 import OdinRecipes from '../components/Recipes/OdinRecipes';
 import Landing from '../components/LandingPage/Landing';
 import DashLanding from '../components/AdminDashboard/DashLanding';
-import WeatherApp from '../components/WeatherApp/WeatherApp';
 
 const AppRoutes = () => {
   const location = useLocation();
