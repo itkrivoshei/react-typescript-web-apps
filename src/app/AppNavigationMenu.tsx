3a8b59d2478e8b337e9f9c44292346d074eb4ac7
Nikita Krivoshei <69799139+itkrivoshei@users.noreply.github.com>
2026-05-25 15:27:32 +0200

    refactor: import navigation styles locally

--- src/app/AppNavigationMenu.tsx
+++ src/app/AppNavigationMenu.tsx
@@ -1,6 +1,8 @@
 import { useState } from 'react';
 import { Link } from 'react-router-dom';
 
+import './AppNavigationMenu.scss';
+
 const AppNavigationMenu = () => {
   const [isOpen, setIsOpen] = useState(false);
 
