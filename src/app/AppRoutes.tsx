77a7a4069e6a2133534687075ddfeffc6752a293
Nikita Krivoshei <69799139+itkrivoshei@users.noreply.github.com>
2026-05-25 15:04:50 +0200

    refactor: move route definitions

---
+++ src/app/AppRoutes.tsx
@@ -0,0 +1,46 @@
+import { Routes, Route, useLocation } from 'react-router-dom';
+
+import Dashboard from '../features/dashboard/Dashboard';
+import Restaurant from '../components/RestaurantPage/Restaurant';
+import TodoApp from '../components/TodoList/TodoApp';
+import TicTacToe from '../components/TicTacToe/TicTacToe';
+import BookLibrary from '../components/LibraryApp/BookLibrary';
+import SignUpForm from '../components/SignUpForm/SignUpForm';
+import Calculator from '../components/Calculator/Calculator';
+import EtchASketch from '../components/EtchASketch/EtchASketch';
+import DrumKit from '../components/DrumKit/DrumKit';
+import RockPaperScissors from '../components/RockPaperScissors/RockPaperScissors';
+import OdinRecipes from '../components/Recipes/OdinRecipes';
+import Landing from '../components/LandingPage/Landing';
+import DashLanding from '../components/AdminDashboard/DashLanding';
+import WeatherApp from '../components/WeatherApp/WeatherApp';
+import AppNavigationMenu from './AppNavigationMenu';
+
+const AppRoutes = () => {
+  const location = useLocation();
+  const showAppsLink = location.pathname !== '/';
+
+  return (
+    <>
+      {showAppsLink && <AppNavigationMenu />}
+      <Routes>
+        <Route path='/' element={<Dashboard />} />
+        <Route path='/Restaurant' element={<Restaurant />} />
+        <Route path='/TodoApp' element={<TodoApp />} />
+        <Route path='/TicTacToe' element={<TicTacToe />} />
+        <Route path='/BookLibrary' element={<BookLibrary />} />
+        <Route path='/SignUpForm' element={<SignUpForm />} />
+        <Route path='/Calculator' element={<Calculator />} />
+        <Route path='/EtchASketch' element={<EtchASketch />} />
+        <Route path='/DrumKit' element={<DrumKit />} />
+        <Route path='/Landing' element={<Landing />} />
+        <Route path='/DashLanding' element={<DashLanding />} />
+        <Route path='/RockPaperScissors' element={<RockPaperScissors />} />
+        <Route path='/OdinRecipes' element={<OdinRecipes />} />
+        <Route path='/WeatherApp' element={<WeatherApp />} />
+      </Routes>
+    </>
+  );
+};
+
+export default AppRoutes;
